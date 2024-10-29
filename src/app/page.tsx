14f9c703095bacf68fe685c65bd5268ab51adb89
awannoman <awannoman287@gmail.com>
2024-10-29 07:08:28 +0500

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 import React from "react";
 const page = () => {
   return (
-    <div className="flex justify-center flex-col items-center h-screen ">
+    <div className="flex justify-center flex-col items-center h-screen bg-black">
       <div className="rounded-full overflow-hidden">
         <Image src={"/picture.jpg"} alt="my picture" width={300} height={300} />
       </div>
-      <div className="m-5">
+      <div className="m-5 text-2xl text-white">
         <h2>This is My Image</h2>
       </div>
     </div>
